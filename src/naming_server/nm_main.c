2dd37af5cceff5e06be8ddbaf993e175cd9d3666
santhoshkumar-git644 <samadhi.yadav@students.iiit.ac.in>
2026-05-10 20:17:09 +0000

    Add Naming Server entry point

---
+++ src/naming_server/nm_main.c
@@ -0,0 +1,47 @@
+#include "common.h"
+#include "logger.h"
+
+int main(int argc, char *argv[]) {
+    init_logger("nm.log");
+    log_message(LOG_INFO, "Naming Server started.");
+
+    // Setup socket to listen for SS and Client connections
+    int server_fd;
+    struct sockaddr_in address;
+    int opt = 1;
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        log_message(LOG_ERROR, "Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        log_message(LOG_ERROR, "Setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(NM_PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        log_message(LOG_ERROR, "Bind failed on port %d", NM_PORT);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        log_message(LOG_ERROR, "Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    log_message(LOG_INFO, "Naming Server listening on port %d", NM_PORT);
+
+    // Main event loop (to be implemented)
+    while (1) {
+        // Accept connections
+        sleep(1);
+    }
+
+    close_logger();
+    return 0;
+}
