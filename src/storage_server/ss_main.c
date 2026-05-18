6f11a13b3bcb1f45f20cd4026d5975608f233892
harshavardhan894 <akulaharshavardhan23@gmail.com>
2026-05-18 08:17:09 +0000

    Spawn client listener thread in SS main

--- src/storage_server/ss_main.c
+++ src/storage_server/ss_main.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "logger.h"
 #include "ss_core.h"
+#include "ss_client_handler.h"
 
 int main(int argc, char *argv[]) {
     init_logger("ss.log");
@@ -14,8 +15,14 @@ int main(int argc, char *argv[]) {
 
     log_message(LOG_INFO, "Storage Server will listen on port %d", ss_port);
     
+    int* client_port_arg = malloc(sizeof(int));
+    *client_port_arg = ss_port + 100; // SS Client port is NM port + 100
+    
+    pthread_t client_thread;
+    pthread_create(&client_thread, NULL, ss_client_listener_thread, client_port_arg);
+    
     // Connect and register with NM
-    ss_init(ss_port);
+    ss_init(*client_port_arg);
 
     // Main loop
     while (1) {
